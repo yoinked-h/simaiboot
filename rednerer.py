f6e240f0cb85af58bab62a2cbed245a6cc5d7e98
yoinked <pepe.dannyboy@gmail.com>
2026-06-03 18:19:08 -0700

    straighgt into the lights

--- rednerer.py
+++ rednerer.py
@@ -614,6 +614,9 @@ from itertools import count
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
 
+@bot.entry_point_command(handler=discord.EntryPointHandler.app_handler, name="launch", description="Launch NotGeki")
+async def launch(ctx: ApplicationContext):
+    await ctx.response.launch_activity()
 
 @dataclass
 class RenderJob:
